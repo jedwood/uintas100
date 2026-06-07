7e1bcc7ae4300ee0b7e752d4d320bc18ea41b2e0
Jed Wood <jed@limechile.com>
2026-06-07 11:02:03 -0600

    Auto-update: 5 new DWR stocking records

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uintas-v1780592312';
+const CACHE_NAME = 'uintas-v1780851723';
 const MAX_CACHE_SIZE = 45 * 1024 * 1024; // Stay under iOS 50MB limit
 
 // Resources to cache immediately
